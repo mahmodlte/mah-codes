bdfb8cec31cde2915fdc9e5710f390884197db9d
mahmodlte <mahmodlte@gmail.com>
2023-06-19 18:21:07 +0300

    Invoked experiences component

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import About from "@/components/About";
+import Experiences from "@/components/Experiences";
 export default function Home() {
   return (
-    <section className="w-full my-20 flex flex-col">
+    <section className="w-full my-20 flex flex-col selection:text-black selection:bg-teal-400">
       <About />
+      <Experiences />
     </section>
   );
 }
