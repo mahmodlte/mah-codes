9f460b6712e1d7bda8638f413d52446975fbe358
mahmodlte <mahmodlte@gmail.com>
2023-06-19 18:20:05 +0300

    Made changes to About

--- components/About.jsx
+++ components/About.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about" className="font-lato flex justify-around">
+    <section
+      id="about"
+      className="font-lato flex flex-col sm:flex-row justify-around"
+    >
       <div>
         <h1 className="dark:text-white text-3xl ">
           Hi there👋️. This is Mahmoud
         </h1>
         <h2 className="text-2xl my-3 text-teal-500 w-80">
-          A Front-End web developer, Tech enthusiast, and photographer at my
-          free time,
+          <span className="text-red-500">{`{`}</span>A Front-End web developer,
+          Tech enthusiast, and photographer at my free time,
+          <span className="text-red-500">{`}`}</span>
         </h2>
       </div>
-      <div className="w-96 dark:text-white text-base">
+      <div className=" sm:w-96 dark:text-white text-base">
         <p className="my-3">
           I grow up in a small village in Mosul, Iraq and from childhood, I was
           immersed by technology. my father got me my first computer at 2004.
@@ -31,7 +35,7 @@ const About = () => {
           the war, I started to read books on Python and C++ tried to learn but
           there was no chance of learning there was no hope for me of getting
           out. In the end the war has ended, and from that time I have started
-          learning coding through an organization called re:coded {`it's`} an
+          learning coding through an organization called Re:Coded {`it's`} an
           orginaztion that teachs youth in confliect affected areas how to code
           and get into the digital economy.
         </p>
