108d5812f1b7fec936b5125f2523c48f2f707db6
mahmodlte <mahmodlte@gmail.com>
2023-06-21 15:51:40 +0300

    added content and made it responsive

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -2,14 +2,14 @@ import Badge from "./Badge";
 
 const Experiences = () => {
   return (
-    <section className=" my-14">
+    <section className=" antialiased h-full my-14">
       <div>
         <h1 className="text-center text-5xl dark:text-white my-16 font-OsWald">
           Experiences
         </h1>
         <hr class="my-12 h-px border-t-0 bg-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-teal-400 opacity-25 dark:opacity-100 bg-gradient-to-r from-transparent via-neutral-900 " />
       </div>
-      <div className="h-full w-full bg-white  rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border dark:border-teal-500">
+      <div className="h-full w-full bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border dark:border-teal-500">
         <div className="m-14">
           <h3 className="text-3xl drop-shadow-2xl dark:text-white font-lato ">
             Scopic Software Oct \ 2021 - Oct \ 2022
@@ -24,7 +24,7 @@ const Experiences = () => {
             sized projects on building the front-end of websites and fixing bugs
             on these websites
           </p>
-          <div className="flex justify-evenly w-2/3 flex-wrap">
+          <div className="sm:flex hidden justify-evenly w-2/3 flex-wrap">
             <Badge title="React" />
             <Badge title="JavaScript" />
             <Badge title="HTML" />
@@ -42,6 +42,30 @@ const Experiences = () => {
           </div>
         </div>
       </div>
+      <div className="my-10 h-full w-full bg-white  rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border dark:border-teal-500">
+        <div className="m-14">
+          <h3 className="text-3xl drop-shadow-2xl dark:text-white font-lato ">
+            Re:Coded 2021
+          </h3>
+          <h4 className="dark:text-white text-2xl">
+            Volunteer Trainer Assistent - React bootcamp
+          </h4>
+          <hr class="my-5 w-56 h-px border-t-0 bg-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-teal-400 opacity-25 dark:opacity-100 bg-gradient-to-r from-transparent via-neutral-900 " />
+
+          <p className="dark:text-white w-2/3 ">
+            In this volunteering opportunity that started at the 2021 React
+            bootcamp. my role was to help trainers team of the bootcamp in
+            prepring online classroom and helping student with the tecnhical
+            issues they face during the bootcamp.
+          </p>
+          <div className="sm:flex hidden justify-evenly w-2/3 flex-wrap"></div>
+        </div>
+      </div>
+      <h4 className="dark:text-white text-2xl">
+        “Obstacles don’t have to stop you. If you run into a wall, don’t turn
+        around and give up. Figure out how to climb it, go through it, or work
+        around it.” – Michael Jordan
+      </h4>
     </section>
   );
 };
