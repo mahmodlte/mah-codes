f2f9fc5c329bc3d46d86cf08fd8ed0c58feed823
mahmodlte <mahmodlte@gmail.com>
2023-06-22 21:19:36 +0300

    projectsList data

---
+++ data/projectsList.js
@@ -0,0 +1,48 @@
+const projects = [
+  {
+    id: 1,
+    name: "Train To Maintain",
+    description: "an app to educate people on health and fitness.",
+    stack: ["React", "Redux", "Tailwind ", "i18next"],
+    github: "https://github.com/Iraq-WBC-Capstones-2020/train-to-maintain-web",
+    demo: "https://traintomaintains.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Movies Finder",
+    description:
+      "A project for those who are looking for their next film to watch.",
+    stack: ["React", "Redux", "Bootstrap"],
+
+    github: "https://github.com/mahmodlte/movies-app",
+    demo: "https://movies-finder-app.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Weatherly",
+    description:
+      "An app for searching and checking the weather in desired locations.",
+    stack: ["React", "NextJs", "Tailwind "],
+
+    github: "https://github.com/mahmodlte/weather-app",
+    demo: "https://weather-app-livid-eta.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "ShareThings",
+    description: "A social media app to share photographs with others. ",
+    stack: ["React", "Sanity", "Tailwind "],
+
+    github: "https://github.com/mahmodlte/sharethings_frontend",
+    demo: "https://sharethings.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "Ai Dev Prompts",
+    stack: ["React", "NextJs", "Tailwind"],
+    github: "https://github.com/mahmodlte/gpt-prompts",
+    description:
+      "app for sharing your AI prompts as developer with other developers",
+  },
+];
+export default projects;
