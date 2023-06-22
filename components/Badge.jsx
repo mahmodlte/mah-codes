aaf1131c1a7570d45f6412604d14c1e6c4cdb297
mahmodlte <mahmodlte@gmail.com>
2023-06-22 21:32:49 +0300

    Changed styles in the badge

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Badge = ({ title }) => {
   return (
-    <div className="select-none mx-2 antialiased hover:bg-teal-500 hover:text-carmine hover:transition-all duration-500 max-w-[95px] my-2 w-full text-center dark:text-teal-400 hover:dark:text-carmine bg-slate-800 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border dark:border-teal-500">
+    <div className="select-none mx-2 antialiased  hover:text-carmine hover:transition-all duration-500 max-w-[95px] my-2 w-full text-center dark:text-teal-400 hover:dark:text-carmine bg-slate-800 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border dark:border-teal-500">
       {title}
     </div>
   );
