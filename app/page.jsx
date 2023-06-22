a655105b72d3047f44759fe613ca75840d149317
mahmodlte <mahmodlte@gmail.com>
2023-06-22 21:23:07 +0300

    Added the components to the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Experiences from "@/components/Experiences";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <section className="w-full my-20 flex flex-col selection:text-black selection:bg-teal-400">
       <About />
       <Experiences />
+      <Skills />
+      <Projects />
+      <Contact />
     </section>
   );
 }
